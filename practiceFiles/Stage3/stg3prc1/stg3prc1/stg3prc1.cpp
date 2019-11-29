e19de30c072c76182ab4968fc0df67dc4f342fc0
Fayetli <lolthedropper@gmail.com>
2019-11-29 23:31:28 +0200

    add stg3prc1

---
+++ practiceFiles/Stage3/stg3prc1/stg3prc1/stg3prc1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include "utilit.h"
+
+using namespace std;
+
+void textHello() {
+	cout << "Hello World!" << endl;
+	cout << "How are you?" << endl;
+}
+void showMenu() {
+	cout << "1.Show detail" << endl;
+	cout << "2.Plus a number" << endl;
+	cout << "3.Minus a number" << endl;
+}
+
+int main() {
+	int choice = 0;
+	int a = 0;
+
+	textHello();
+
+	while (1) {
+		showMenu();
+		cin >> choice;
+
+		switch (choice) {
+		case 1:
+			cout << "Your number: " << a << endl;
+			break;
+		case 2:
+			addNumber(&a);
+			break;
+		case 3:
+			minusNumber(&a);
+			break;
+		default:
+			cout << "Bad choice." << endl;
+		}
+	}
+
+}
+
+void addNumber(int* a) {
+	*a += 2;
+
+}
+void minusNumber(int* a) {
+	*a -= 2;
+}
---
+++ practiceFiles/Stage3/stg3prc1/stg3prc1/utilit.h
@@ -0,0 +1,10 @@
+#ifndef UTILIT_H_
+#define UTILIT_H_
+
+
+void addNumber(int* a);
+void minusNumber(int* a);
+
+
+
+#endif
